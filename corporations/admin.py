72717973310be5fb2ddb33c36566a3df39914d14
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-08-23 01:06:58 +0900

    Feat : corporations 모델 생성, 어드민 등록 및 migrations

--- corporations/admin.py
+++ corporations/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from corporations.models import (
+    Country,
+    Region,
+    Corporation,
+    TechStack,
+    Position,
+    Recruitment,
+    Recruiter,
+    )
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(Corporation)
+admin.site.register(TechStack)
+admin.site.register(Position)
+admin.site.register(Recruitment)
+admin.site.register(Recruiter)
